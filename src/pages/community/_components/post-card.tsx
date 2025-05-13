1be423cf30e62b089747d5bbd8b12f971f454cd7
igor <pythagoras1206@gmail.com>
2025-05-13 10:49:06 +0900

    minor adjustments

--- src/pages/community/_components/post-card.tsx
+++ src/pages/community/_components/post-card.tsx
@@ -34,7 +34,7 @@ function PostCard({ post, variant = 'small', maxComments = 1 }: PostCardProps) {
   });
 
   const authorName = `${post?.author?.firstName} ${post?.author?.lastName}`;
-  const authorInitials = getInitials(authorName);
+  // const authorInitials = getInitials(authorName);
 
   if (loading && !data) {
     return (
@@ -48,7 +48,7 @@ function PostCard({ post, variant = 'small', maxComments = 1 }: PostCardProps) {
     return (
       <div className="block w-full h-full border border-[#E9E9E9] rounded-[20px] overflow-hidden p-6">
         <div className="relative h-full flex flex-col">
-          <div className="w-full aspect-video bg-gradient-to-r from-purple-300 to-blue-300 flex-shrink-0 rounded-xl overflow-hidden">
+          <div className="w-full aspect-video bg-gradient-to-r from-purple-300 to-blue-300 flex-shrink-0 rounded-lg overflow-hidden">
             {post?.image ? (
               <img
                 src={post.image}
@@ -62,12 +62,20 @@ function PostCard({ post, variant = 'small', maxComments = 1 }: PostCardProps) {
             )}
           </div>
 
-          <div className="p-5 flex-grow flex flex-col justify-between">
+          <div className="pt-3 flex-grow flex flex-col justify-between">
             <div>
-              <div className="flex justify-between items-center mb-4">
-                <div className="flex items-center">
-                  <h2 className="font-bold text-lg">{title || 'Community'}</h2>
-                </div>
+              <h2 className="font-bold text-base mb-0.5">{title || 'Community'}</h2>
+
+              <p className="text-xs mb-0.5 text-muted-foreground font-bold">{authorName}</p>
+              <div className="flex items-center mb-2 justify-between">
+                <p className="text-xs text-muted-foreground">
+                  {createdAt ? format(new Date(createdAt), 'yyyy.MM.dd') : ''}
+                  {isSponsor && (
+                    <Link to={`/community/${post?.id}/edit`} className="ml-2">
+                      <Settings className="w-4 h-4 inline" />
+                    </Link>
+                  )}
+                </p>
                 <div className="flex gap-1 flex-wrap">
                   {keywords?.slice(0, 3).map((k, i) => (
                     <Badge
@@ -86,27 +94,10 @@ function PostCard({ post, variant = 'small', maxComments = 1 }: PostCardProps) {
                 </div>
               </div>
 
-              <div className="flex items-center mb-2">
-                <Avatar className="w-8 h-8 mr-3">
-                  <AvatarImage src="" alt={authorName} />
-                  <AvatarFallback>{authorInitials}</AvatarFallback>
-                </Avatar>
-                <p className="text-sm">{authorName}</p>
-                <span className="mx-2 text-gray-400">•</span>
-                <p className="text-sm text-gray-500">
-                  {createdAt ? format(new Date(createdAt), 'yyyy.MM.dd') : ''}
-                  {isSponsor && (
-                    <Link to={`/community/${post?.id}/edit`} className="ml-2">
-                      <Settings className="w-4 h-4 inline" />
-                    </Link>
-                  )}
-                </p>
-              </div>
-
               <p className="text-sm text-gray-700 mb-6 line-clamp-3">{content}</p>
             </div>
 
-            <div className="border-t border-gray-100 pt-4 pb-1">
+            <div className="border-t border-gray-100 pt-6 pb-1">
               <h3 className="font-medium text-sm mb-4">New comment</h3>
 
               {data?.commentsByPost?.slice(0, maxComments).map((comment) => (
@@ -139,7 +130,7 @@ function PostCard({ post, variant = 'small', maxComments = 1 }: PostCardProps) {
               <div className="flex justify-end">
                 <Link
                   to={`/community/posts/${id}`}
-                  className="flex items-center text-sm font-medium text-gray-600 hover:text-gray-800"
+                  className="flex items-center text-sm font-medium text-foreground hover:text-gray-800"
                 >
                   View more <ArrowRight className="w-4 h-4 ml-1" />
                 </Link>
@@ -153,10 +144,10 @@ function PostCard({ post, variant = 'small', maxComments = 1 }: PostCardProps) {
 
   // Default small card
   return (
-    <div className="block w-full h-full border border-[#E9E9E9] rounded-[20px] overflow-hidden p-6">
+    <div className="block w-full border border-[#E9E9E9] rounded-[20px] overflow-hidden p-6">
       <div className="relative h-full flex flex-col">
-        <div className="flex gap-4">
-          <div className="aspect-video w-1/2 bg-gradient-to-r from-purple-300 to-blue-300 flex-shrink-0 rounded-xl overflow-hidden">
+        <div className="flex gap-4 mb-5">
+          <div className="aspect-video w-1/2 bg-gradient-to-r from-purple-300 to-blue-300 flex-shrink-0 rounded-lg overflow-hidden">
             {post?.image ? (
               <img src={post.image} alt={title || 'Post'} className=" object-cover" />
             ) : (
@@ -168,7 +159,7 @@ function PostCard({ post, variant = 'small', maxComments = 1 }: PostCardProps) {
 
           <div>
             <div className="flex justify-between items-center mb-2">
-              <h2 className="font-bold text-base">{title || 'Community'}</h2>
+              <h2 className="font-bold text-base line-clamp-1">{title || 'Community'}</h2>
               {/* <div className="flex gap-1">
                 {keywords?.slice(0, 1).map((k, i) => (
                   <Badge
@@ -184,13 +175,12 @@ function PostCard({ post, variant = 'small', maxComments = 1 }: PostCardProps) {
               </div> */}
             </div>
 
-            <div className="flex items-center mb-1">
+            <p className="text-xs font-bold text-muted-foreground">{authorName}</p>
+            <div className="flex items-center justify-between mb-3">
               {/* <Avatar className="w-4 h-4 mr-1.5">
                 <AvatarImage src={post?.author?.image || ''} alt={authorName} />
                 <AvatarFallback className="text-[8px]">{authorInitials}</AvatarFallback>
               </Avatar> */}
-              <p className="text-xs">{authorName}</p>
-              <span className="mx-1 text-gray-400 text-xs">•</span>
               <p className="text-xs text-gray-500 mr-3">
                 {createdAt ? format(new Date(createdAt), 'yyyy.MM.dd') : ''}
               </p>
@@ -209,18 +199,20 @@ function PostCard({ post, variant = 'small', maxComments = 1 }: PostCardProps) {
               </div>
             </div>
 
-            <p className="text-xs text-gray-700 mb-3 line-clamp-2">{content}</p>
+            <p className="text-sm text-gray-700 mb-3 line-clamp-4">{content}</p>
           </div>
         </div>
 
-        <div className="p-4 flex-grow flex flex-col justify-between">
+        <div className="pt-4 flex-grow flex flex-col justify-between">
           {data?.commentsByPost?.length && data?.commentsByPost?.length > 0 && (
-            <div className="border-t border-gray-100 pt-2 pb-1">
-              <h3 className="font-medium text-xs mb-2">New comment</h3>
+            <div className="border-t pt-5 border-gray-100 pb-1">
+              <span className="px-[10px] py-[2px] mb-4 font-semibold text-secondary-foreground bg-secondary rounded-full text-xs inline-block">
+                New comment
+              </span>
 
               {data?.commentsByPost?.slice(0, maxComments).map((comment) => (
                 <div key={comment.id} className="flex mb-2">
-                  <Avatar className="w-6 h-6 mr-2">
+                  <Avatar className="w-10 h-10 mr-2">
                     <AvatarImage
                       src={comment.author?.image || ''}
                       alt={`${comment.author?.firstName} ${comment.author?.lastName}`}
@@ -232,8 +224,8 @@ function PostCard({ post, variant = 'small', maxComments = 1 }: PostCardProps) {
                     </AvatarFallback>
                   </Avatar>
                   <div>
-                    <div className="flex items-center mb-0.5">
-                      <h4 className="font-medium text-xs">{`${comment.author?.firstName || ''} ${comment.author?.lastName || ''}`}</h4>
+                    <div className="flex items-center mb-1 justify-between">
+                      <h4 className="font-medium text-sm">{`${comment.author?.firstName || ''} ${comment.author?.lastName || ''}`}</h4>
                       <span className="text-xs text-gray-500 ml-2">
                         {comment.createdAt
                           ? format(new Date(comment.createdAt), 'MMM dd, yyyy')
@@ -245,12 +237,12 @@ function PostCard({ post, variant = 'small', maxComments = 1 }: PostCardProps) {
                 </div>
               ))}
 
-              <div className="flex justify-end">
+              <div className="flex justify-end mt-4">
                 <Link
                   to={`/community/posts/${id}`}
-                  className="flex items-center text-xs font-medium text-gray-600 hover:text-gray-800"
+                  className="flex items-center text-sm font-medium text-foreground hover:text-gray-800"
                 >
-                  View more <ArrowRight className="w-3 h-3 ml-1" />
+                  View more <ArrowRight className="w-4 h-4 ml-1" />
                 </Link>
               </div>
             </div>
