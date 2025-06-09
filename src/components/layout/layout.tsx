743dfbdeb740a44b05d0525566429f8f97dd3cd9
juniahn-dev <gud0415@gmail.com>
2025-06-09 15:05:21 -0700

    [feat] Add Mobile support page

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,19 +1,48 @@
+import loadingGif from '@/assets/icons/loading.gif';
 import Footer from '@/components/layout/footer';
 import Header from '@/components/layout/header';
 import Sidebar from '@/components/layout/sidebar';
+import { useEffect, useState } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { Outlet } from 'react-router';
+import MobileWebView from '../mobile-web-view';
 
 function Layout() {
+  const [isMobile, setIsMobile] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent || navigator.vendor;
+    const isMobileDevice = /android|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(
+      userAgent,
+    );
+    setIsMobile(isMobileDevice);
+    setLoading(false);
+  }, []);
+
   return (
     <>
-      <Sidebar />
-      <main className="bg-white rounded-2xl m-3 ml-[240px]">
-        <Header />
-        <Outlet />
-        <Footer />
-      </main>
-      <Toaster />
+      {loading ? (
+        <div className="flex items-center justify-center h-screen">
+          <img className="w-[30px]" src={loadingGif} alt="loading" />
+        </div>
+      ) : (
+        <>
+          {isMobile ? (
+            <MobileWebView />
+          ) : (
+            <>
+              <Sidebar />
+              <main className="bg-white rounded-2xl m-3 ml-[240px]">
+                <Header />
+                <Outlet />
+                <Footer />
+              </main>
+            </>
+          )}
+          <Toaster />
+        </>
+      )}
     </>
   );
 }
