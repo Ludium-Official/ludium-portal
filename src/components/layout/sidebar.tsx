11460857beed592b0f024835b3caedbc60a4488f
Murat <renja.dizz@gmail.com>
2025-07-02 19:29:26 +0900

    update users

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,26 +1,19 @@
 import logo from '@/assets/logo.svg';
 import { useAuth } from '@/lib/hooks/use-auth';
-import { CircleAlert, Scroll, UserRound, Users } from 'lucide-react';
-
-import { useRef, useState } from 'react';
-import { Link, NavLink, useLocation } from 'react-router';
+import { CircleAlert, MessageCircle, Scroll, UserRound, Users } from 'lucide-react';
+import { Link, NavLink } from 'react-router';
 
 const Sidebar = () => {
   const { isLoggedIn } = useAuth();
-  const location = useLocation();
-  const [communityMenuOpen, setCommunityMenuOpen] = useState(false);
-  const closeTimeout = useRef<NodeJS.Timeout | null>(null);
+
   const links = [
     { name: 'Programs', path: '/programs', icon: Scroll },
     {
       name: 'Community',
       path: '/community',
-      icon: Users,
-      submenu: [
-        { name: 'Community', path: '/community', icon: Users },
-        { name: 'Users', path: '/users', icon: UserRound },
-      ],
+      icon: MessageCircle,
     },
+    { name: 'Users', path: '/users', icon: Users },
   ];
 
   if (isLoggedIn) {
@@ -35,68 +28,20 @@ const Sidebar = () => {
       <nav>
         <ul className="space-y-5 mx-2">
           {links.map((link) => (
-            <li
-              key={link.path}
-              className="relative z-50"
-              onMouseEnter={() => {
-                if (link.submenu) {
-                  if (closeTimeout.current) clearTimeout(closeTimeout.current);
-                  setCommunityMenuOpen(true);
-                }
-              }}
-              onMouseLeave={() => {
-                if (link.submenu) {
-                  closeTimeout.current = setTimeout(() => {
-                    setCommunityMenuOpen(false);
-                  }, 120); // 120ms delay
-                }
-              }}
-            >
-              {link.submenu ? (
-                <>
-                  <p
-                    // to={link.path}
-                    className={`group flex gap-4 items-center px-4 py-[14px] rounded-xl transition-all text-[18px] font-medium cursor-default ${location.pathname.startsWith('/community') ||
-                        location.pathname.startsWith('/users')
-                        ? 'bg-primary-light text-primary'
-                        : 'hover:bg-primary-light hover:text-primary'
-                      }`}
-                  >
-                    <link.icon className="group-active:text-primary group-hover:text-primary" />
-                    {link.name}
-                  </p>
-                  {communityMenuOpen && (
-                    <ul className="absolute z-50 left-full -top-1/2 ml-2 w-48 bg-white rounded-xl shadow-lg p-3 space-y-2">
-                      {link.submenu.map((sublink) => (
-                        <li key={sublink.path}>
-                          <NavLink
-                            to={sublink.path}
-                            className={
-                              'flex gap-3 items-center px-4 py-3 rounded-lg transition-all text-[16px] font-medium hover:bg-primary-light hover:text-primary'
-                            }
-                          >
-                            <sublink.icon className="group-active:text-primary group-hover:text-primary" />
-                            {sublink.name}
-                          </NavLink>
-                        </li>
-                      ))}
-                    </ul>
-                  )}
-                </>
-              ) : (
-                <NavLink
-                  to={link.path}
-                  className={({ isActive }) =>
-                    `group flex gap-4 items-center px-4 py-[14px] rounded-xl transition-all text-[18px] font-medium ${isActive
+            <li key={link.path} className="relative z-50">
+              <NavLink
+                to={link.path}
+                className={({ isActive }) =>
+                  `group flex gap-4 items-center px-4 py-[14px] rounded-xl transition-all text-[18px] font-medium ${
+                    isActive
                       ? 'bg-primary-light text-primary'
                       : 'hover:bg-primary-light hover:text-primary'
-                    }`
-                  }
-                >
-                  <link.icon className="group-active:text-primary group-hover:text-primary" />
-                  {link.name}
-                </NavLink>
-              )}
+                  }`
+                }
+              >
+                <link.icon className="group-active:text-primary group-hover:text-primary" />
+                {link.name}
+              </NavLink>
             </li>
           ))}
           <li>
--- src/pages/community/users/index.tsx
+++ src/pages/community/users/index.tsx
@@ -3,14 +3,15 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Badge } from '@/components/ui/badge';
 import { Input } from '@/components/ui/input';
 import { Pagination } from '@/components/ui/pagination';
+import { Separator } from '@/components/ui/separator';
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { getInitials, getUserName } from '@/lib/utils';
+import { getInitials } from '@/lib/utils';
 import { SortEnum } from '@/types/types.generated';
-import { ArrowRight } from 'lucide-react';
+import { BriefcaseBusinessIcon, Building2Icon, SearchIcon } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { Link, useSearchParams } from 'react-router';
 
-const userPageSize = 6;
+const userPageSize = 12;
 
 function UsersPage() {
   const [selectedTab, setSelectedTab] = useState('all');
@@ -43,11 +44,11 @@ function UsersPage() {
           },
           ...(selectedTab === 'by-projects'
             ? [
-              {
-                field: 'byNumberOfProjects',
-                value: 'asc',
-              },
-            ]
+                {
+                  field: 'byNumberOfProjects',
+                  value: 'asc',
+                },
+              ]
             : []),
         ],
       },
@@ -55,23 +56,30 @@ function UsersPage() {
   });
 
   return (
-    <div className="pb-10">
-      <div className="bg-white p-4">
-        <div className="flex justify-between items-center mb-9">
-          <h3 className="text-gray-dark font-bold text-xl">Users</h3>
+    <div className="pb-[60px] flex flex-col gap-10 rounded-b-[16px] bg-white">
+      <div className="px-10">
+        <div className="flex justify-between items-center pt-9 pb-4">
+          <h3 className="text-gray-dark font-bold text-3xl">Agent</h3>
         </div>
 
-        <div className="flex justify-between items-center mb-6">
-          <Tabs value={selectedTab} onValueChange={setSelectedTab}>
-            <TabsList>
-              <TabsTrigger value="all">All</TabsTrigger>
-              <TabsTrigger value="by-projects">By number of projects</TabsTrigger>
+        <div className="flex justify-between items-center py-[14px]">
+          <Tabs value={selectedTab} onValueChange={setSelectedTab} className="h-10 rounded-md">
+            <TabsList className="rounded-md">
+              <TabsTrigger value="all" className="rounded-xs px-4">
+                All
+              </TabsTrigger>
+              <TabsTrigger value="by-projects" className="rounded-xs px-4">
+                By number of projects
+              </TabsTrigger>
             </TabsList>
           </Tabs>
 
-          <div className="flex items-center gap-3">
+          <div className="flex items-center relative px-0">
+            <div className="absolute bottom-0 left-3 top-0 my-auto flex items-center justify-center">
+              <SearchIcon color="#71717A" width={16} height={16} strokeWidth={2} />
+            </div>
             <Input
-              className="w-[432px] rounded-md h-10"
+              className="w-[360px] rounded-md h-10 bg-slate-50 pl-8"
               placeholder="Search..."
               value={searchParams.get('search') ?? ''}
               onChange={(e) => {
@@ -85,26 +93,15 @@ function UsersPage() {
           </div>
         </div>
 
-        <div className="grid grid-cols-2 gap-5">
+        <div className="grid grid-cols-2 gap-3 py-6">
           {usersData?.users?.data?.map((user) => (
-            <div key={user.id} className="border rounded-md p-6">
-              <div className="flex items-center justify-between mb-2">
-                <div className="flex gap-2">
-                  <Badge className="bg-red-500 px-2.5 py-0.5">BD</Badge>
-                  <Badge className="bg-primary px-2.5 py-0.5">Developer</Badge>
-                  <Badge className="bg-pink-300 px-2.5 py-0.5">Solidity</Badge>
-                </div>
-
-                <Link
-                  to={`/users/${user.id}`}
-                  className="flex gap-2 text-sm items-center font-medium"
-                >
-                  See details <ArrowRight className="w-4 h-4" />
-                </Link>
-              </div>
-
-              <div className="gap-4 flex items-center mb-6">
-                <Avatar className="w-[64px] h-[64px]">
+            <Link
+              to={`/users/${user.id}`}
+              key={user.id}
+              className="flex flex-col gap-3 border rounded-lg p-5"
+            >
+              <div className="flex gap-4 items-center">
+                <Avatar className="w-[60px] h-[60px]">
                   <AvatarImage
                     src={user?.image || ''}
                     alt={`${user?.firstName} ${user?.lastName}`}
@@ -113,16 +110,74 @@ function UsersPage() {
                     {getInitials(`${user?.firstName || ''} ${user?.lastName || ''}`)}
                   </AvatarFallback>
                 </Avatar>
-
-                <p className="text-lg font-bold">{getUserName(user)}</p>
+                <div className="flex flex-col gap-2">
+                  <p className="font-bold text-lg text-gray-dark">{`${user?.firstName} ${user?.lastName}`}</p>
+                  <div className="flex gap-[3.33px] items-center justify-start">
+                    <Building2Icon width={16} height={16} color="#71717A" />
+                    <p className="text-sm text-zinc-500">{user.organizationName}</p>
+                  </div>
+                  <div className="flex gap-[3.33px] items-center justify-start">
+                    <BriefcaseBusinessIcon width={16} height={16} color="#71717A" />
+                    <div className="flex gap-[6px]">
+                      <Badge className="bg-gray-dark px-2.5 py-0.5">BD</Badge>
+                      <Badge className="bg-gray-dark px-2.5 py-0.5">Developer</Badge>
+                      <Badge className="bg-gray-dark px-2.5 py-0.5">Solidity</Badge>
+                    </div>
+                  </div>
+                </div>
               </div>
+              <div className="flex gap-2 items-center justify-between">
+                <p className="line-clamp-6 text-sm text-gray-dark font-inter">{user.about}</p>
+                <div className="flex gap-3">
+                  <div className="p-3 flex flex-col gap-1">
+                    <p className="font-bold text-xs text-gray-dark">Recruitment</p>
+                    <Separator />
+                    <div className="flex justify-between">
+                      <p className="font-bold text-xs text-zinc-500">Sponsor</p>
+                      <p className="font-bold text-xs text-gray-dark">3</p>
+                    </div>
+                    <Separator />
+                    <div className="flex justify-between">
+                      <p className="font-bold text-xs text-zinc-500">Validator</p>
+                      <p className="font-bold text-xs text-gray-dark">4</p>
+                    </div>
+                    <Separator />
+                    <div className="flex justify-between">
+                      <p className="font-bold text-xs text-zinc-500">Builder</p>
+                      <p className="font-bold text-xs text-gray-dark">2</p>
+                    </div>
+                  </div>
 
-              <h4 className="text-sm font-bold text-foreground">Summary</h4>
-              <p className="text-sm">{user.summary}</p>
-            </div>
+                  <div className="p-3 flex flex-col gap-1">
+                    <p className="font-bold text-xs text-gray-dark">Investment</p>
+                    <Separator />
+                    <div className="flex justify-between">
+                      <p className="font-bold text-xs text-zinc-500">Host</p>
+                      <p className="font-bold text-xs text-gray-dark">3</p>
+                    </div>
+                    <Separator />
+                    <div className="flex justify-between">
+                      <p className="font-bold text-xs text-zinc-500">Project</p>
+                      <p className="font-bold text-xs text-gray-dark">4</p>
+                    </div>
+                    <Separator />
+                    <div className="flex justify-between">
+                      <p className="font-bold text-xs text-zinc-500">Suppoter</p>
+                      <p className="font-bold text-xs text-gray-dark">2</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex gap-[6px]">
+                <Badge className="bg-zinc-100 text-gray-dark px-2.5 py-0.5">Crypto</Badge>
+                <Badge className="bg-zinc-100 text-gray-dark px-2.5 py-0.5">BD</Badge>
+                <Badge className="bg-zinc-100 text-gray-dark px-2.5 py-0.5">Develope</Badge>
+              </div>
+            </Link>
           ))}
         </div>
       </div>
+
       <Pagination totalCount={usersData?.users?.count ?? 0} pageSize={userPageSize} />
     </div>
   );
