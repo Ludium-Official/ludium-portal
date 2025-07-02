df96ec1b9dd037ac6acdeedd3f24397c7c6b3b99
Murat <renja.dizz@gmail.com>
2025-07-02 19:46:34 +0900

    fix merge error

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,19 +1,41 @@
 import logo from '@/assets/logo.svg';
 import { useAuth } from '@/lib/hooks/use-auth';
-import { CircleAlert, MessageCircle, Scroll, ShieldCheck, UserRound, Users } from 'lucide-react';
-import { Link, NavLink } from 'react-router';
+import {
+  CircleAlert,
+  type LucideIcon,
+  MessageCircle,
+  Scroll,
+  ShieldCheck,
+  UserRound,
+  Users,
+} from 'lucide-react';
+import { useRef, useState } from 'react';
+import { Link, NavLink, useLocation } from 'react-router';
+
+type SidebarLink = {
+  name: string;
+  path: string;
+  icon: LucideIcon;
+  submenu?: {
+    name: string;
+    path: string;
+    icon: LucideIcon;
+  }[];
+};
 
 const Sidebar = () => {
   const { isLoggedIn, isAdmin } = useAuth();
-
-  const links = [
+  const location = useLocation();
+  const [communityMenuOpen, setCommunityMenuOpen] = useState(false);
+  const closeTimeout = useRef<NodeJS.Timeout | null>(null);
+  const links: SidebarLink[] = [
     { name: 'Programs', path: '/programs', icon: Scroll },
     {
       name: 'Community',
       path: '/community',
       icon: MessageCircle,
     },
-    { name: 'Users', path: '/users', icon: Users },
+    { name: 'Agent', path: '/users', icon: Users },
   ];
 
   if (isLoggedIn) {
@@ -49,11 +71,12 @@ const Sidebar = () => {
                 <>
                   <p
                     // to={link.path}
-                    className={`group flex gap-4 items-center px-4 py-[14px] rounded-xl transition-all text-[18px] font-medium cursor-default ${location.pathname.startsWith('/community') ||
+                    className={`group flex gap-4 items-center px-4 py-[14px] rounded-xl transition-all text-[18px] font-medium cursor-default ${
+                      location.pathname.startsWith('/community') ||
                       location.pathname.startsWith('/users')
-                      ? 'bg-primary-light text-primary'
-                      : 'hover:bg-primary-light hover:text-primary'
-                      }`}
+                        ? 'bg-primary-light text-primary'
+                        : 'hover:bg-primary-light hover:text-primary'
+                    }`}
                   >
                     <link.icon className="group-active:text-primary group-hover:text-primary" />
                     {link.name}
@@ -80,15 +103,17 @@ const Sidebar = () => {
                 <NavLink
                   to={link.path}
                   className={({ isActive }) =>
-                    `group flex gap-4 items-center px-4 py-[14px] rounded-xl transition-all text-[18px] font-medium ${isActive
-                      ? 'bg-primary-light text-primary'
-                      : 'hover:bg-primary-light hover:text-primary'
-                  }`
-                }
-              >
-                <link.icon className="group-active:text-primary group-hover:text-primary" />
-                {link.name}
-              </NavLink>
+                    `group flex gap-4 items-center px-4 py-[14px] rounded-xl transition-all text-[18px] font-medium ${
+                      isActive
+                        ? 'bg-primary-light text-primary'
+                        : 'hover:bg-primary-light hover:text-primary'
+                    }`
+                  }
+                >
+                  <link.icon className="group-active:text-primary group-hover:text-primary" />
+                  {link.name}
+                </NavLink>
+              )}
             </li>
           ))}
           <li>
@@ -104,7 +129,10 @@ const Sidebar = () => {
           </li>
           {isAdmin && (
             <li>
-              <NavLink to="/admin" className="group flex gap-4 items-center px-4 py-[14px] rounded-xl transition-all text-[18px] font-medium hover:bg-primary-light hover:text-primary">
+              <NavLink
+                to="/admin"
+                className="group flex gap-4 items-center px-4 py-[14px] rounded-xl transition-all text-[18px] font-medium hover:bg-primary-light hover:text-primary"
+              >
                 <ShieldCheck />
                 Admin
               </NavLink>
