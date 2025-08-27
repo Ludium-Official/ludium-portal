765f3679064fdec3f9e6bae234569ff9d805bdb1
igor <pythagoras1206@gmail.com>
2025-08-27 12:23:04 +0900

    reclaim investments

--- src/pages/community/users/_components/investment-reclaim-tab.tsx
+++ src/pages/community/users/_components/investment-reclaim-tab.tsx
@@ -1,8 +1,11 @@
+import { useReclaimInvestmentMutation } from '@/apollo/mutation/reclaim-investment.generated';
+import { useInvestmentsQuery } from '@/apollo/queries/investments.generated';
 import { useProfileQuery } from '@/apollo/queries/profile.generated';
-import { useProgramsQuery } from '@/apollo/queries/programs.generated';
-import UsdtIcon from '@/assets/icons/crypto/usdt';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { Pagination } from '@/components/ui/pagination';
+import notify from '@/lib/notify';
+import { getCurrencyIcon } from '@/lib/utils';
 import { Search } from 'lucide-react';
 import { useState } from 'react';
 import { useParams, useSearchParams } from 'react-router';
@@ -11,7 +14,7 @@ import { AgentBreadcrumbs } from './agent-breadcrumbs';
 const programPageSize = 6;
 
 export default function UserInvestmentReclaimTab({ myProfile }: { myProfile?: boolean }) {
-  const { id, role } = useParams();
+  const { id } = useParams();
   const [searchParams, _setSearchParams] = useSearchParams();
   const [searchQuery, setSearchQuery] = useState('');
   const currentPage = Number(searchParams.get('page')) || 1;
@@ -23,31 +26,34 @@ export default function UserInvestmentReclaimTab({ myProfile }: { myProfile?: bo
 
   const profileId = myProfile ? profileData?.profile?.id ?? '' : id ?? '';
 
-  const { data: programData } = useProgramsQuery({
+  const { data: investmentData, refetch } = useInvestmentsQuery({
     variables: {
+      supporterId: profileId,
       pagination: {
         limit: programPageSize,
         offset: (currentPage - 1) * programPageSize,
-
-        filter: [
-          {
-            value: profileId,
-            field: role ?? '',
-          },
-          ...(searchQuery
-            ? [
-              {
-                field: 'name',
-                value: searchQuery,
-              },
-            ]
-            : []),
-        ],
       },
     },
-    skip: !profileId,
   });
-  console.log("🚀 ~ UserRecruitmentReclaimTab ~ programData:", programData)
+
+  const [reclaimInvestment] = useReclaimInvestmentMutation();
+
+  const handleReclaimInvestment = async (investmentId: string, txHash: string) => {
+
+    await reclaimInvestment({
+      variables: {
+        input: { investmentId, txHash: txHash ?? '' },
+      },
+      onCompleted: () => {
+        notify('Investment reclaimed successfully', 'success');
+        refetch();
+      },
+      onError: (error) => {
+        notify(`Failed to reclaim investment: ${error.message}`, 'error');
+      },
+    });
+  };
+
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(e.target.value);
   };
@@ -68,131 +74,55 @@ export default function UserInvestmentReclaimTab({ myProfile }: { myProfile?: bo
           </div>
         </div>
         <div className="flex flex-col gap-3">
-          <div className='p-5 border rounded-lg w-full'>
-            <div className='bg-[#18181B0A] rounded-full px-[10px] inline-flex items-center gap-2 mb-4'>
-              <span className='w-[14px] h-[14px] rounded-full bg-red-500 block' />
-              <p className='text-secondary-foreground text-sm font-semibold'>
-                Project Failed
-              </p>
-            </div>
-
-            <div className='flex items-center gap-2 mb-3'>
-              <div className='w-8 h-8 rounded-full bg-gray-200' />
-              <div className='flex flex-col gap-1'>
-                <p className='text-sm font-semibold'>
-                  Project Name
+          {investmentData?.investments?.data?.map((investment) => (
+            <div className='p-5 border rounded-lg w-full' key={investment?.id}>
+              <div className='bg-[#18181B0A] rounded-full px-[10px] inline-flex items-center gap-2 mb-4'>
+                <span className='w-[14px] h-[14px] rounded-full bg-red-500 block' />
+                <p className='text-secondary-foreground text-sm font-semibold'>
+                  Refund
                 </p>
               </div>
-            </div>
 
-            <div className='flex items-center justify-between px-2 py-1.5 bg-[#18181B0A] rounded-lg mb-2'>
-              <p className='text-sm font-medium text-neutral-400'>PAYED</p>
-              <div className='flex items-center gap-1'>
-                <p className='text-sm text-muted-foreground font-bold'>2000</p>
-                <UsdtIcon width={16} height={16} />
-                <p className='text-sm text-muted-foreground font-medium'>USDT</p>
+              <div className='flex items-center gap-2 mb-3'>
+                <div className='w-8 h-8 rounded-full bg-gray-200' />
+                <div className='flex flex-col gap-1'>
+                  <p className='text-sm font-semibold'>
+                    {investment?.project?.name}
+                  </p>
+                </div>
+              </div>
+
+              <div className='flex items-center justify-between px-2 py-1.5 bg-[#18181B0A] rounded-lg mb-2'>
+                <p className='text-sm font-medium text-neutral-400'>PAYED</p>
+                <div className='flex items-center gap-2'>
+                  <p className='text-sm text-muted-foreground font-bold'>{investment?.amount}</p>
+                  {getCurrencyIcon(investment?.project?.program?.currency ?? '')}
+                  <p className='text-sm text-muted-foreground font-medium'>{investment?.project?.program?.currency}</p>
+                </div>
+              </div>
+
+              <div className='flex items-center justify-between px-2 py-2.5 bg-[#18181B0A] rounded-lg mb-4'>
+                <p className='text-sm font-bold text-foreground'>RECLAIM</p>
+                <div className='flex items-center gap-2'>
+                  <p className='text-xl text-primary font-bold'>{investment?.amount}</p>
+                  {getCurrencyIcon(investment?.project?.program?.currency ?? '')}
+                  <p className='text-sm text-muted-foreground font-medium'>{investment?.project?.program?.currency}</p>
+                </div>
+              </div>
+
+              <div className='flex items-center justify-end'>
+                <Button size='sm' className='px-6' onClick={() => handleReclaimInvestment(investment?.id ?? '', investment?.txHash ?? '')}>Reclaim now</Button>
               </div>
             </div>
+          ))}
 
-            <div className='flex items-center justify-between px-2 py-2.5 bg-[#18181B0A] rounded-lg mb-4'>
-              <p className='text-sm font-bold text-foreground'>RECLAIM</p>
-              <div className='flex items-center gap-1'>
-                <p className='text-xl text-primary font-bold'>1000</p>
-                <UsdtIcon width={16} height={16} />
-                <p className='text-sm text-muted-foreground font-medium'>USDT</p>
-              </div>
-            </div>
 
-            <div className='flex items-center justify-end'>
 
-              <Button size='sm' className='px-6'>Reclaim now</Button>
-            </div>
-          </div>
 
-          <div className='p-5 border rounded-lg w-full'>
-            <div className='bg-[#18181B0A] rounded-full px-[10px] inline-flex items-center gap-2 mb-4'>
-              <span className='w-[14px] h-[14px] rounded-full bg-red-500 block' />
-              <p className='text-secondary-foreground text-sm font-semibold'>
-                Project Failed
-              </p>
-            </div>
 
-            <div className='flex items-center gap-2 mb-3'>
-              <div className='w-8 h-8 rounded-full bg-gray-200' />
-              <div className='flex flex-col gap-1'>
-                <p className='text-sm font-semibold'>
-                  Project Name
-                </p>
-              </div>
-            </div>
-
-            <div className='flex items-center justify-between px-2 py-1.5 bg-[#18181B0A] rounded-lg mb-2'>
-              <p className='text-sm font-medium text-neutral-400'>PAYED</p>
-              <div className='flex items-center gap-1'>
-                <p className='text-sm text-muted-foreground font-bold'>2000</p>
-                <UsdtIcon width={16} height={16} />
-                <p className='text-sm text-muted-foreground font-medium'>USDT</p>
-              </div>
-            </div>
-
-            <div className='flex items-center justify-between px-2 py-2.5 bg-[#18181B0A] rounded-lg mb-4'>
-              <p className='text-sm font-bold text-foreground'>RECLAIM</p>
-              <div className='flex items-center gap-1'>
-                <p className='text-xl text-primary font-bold'>1000</p>
-                <UsdtIcon width={16} height={16} />
-                <p className='text-sm text-muted-foreground font-medium'>USDT</p>
-              </div>
-            </div>
-
-            <div className='flex items-center justify-end'>
-
-              <Button size='sm' className='px-6'>Reclaim now</Button>
-            </div>
-          </div>
-
-          <div className='p-5 border rounded-lg w-full'>
-            <div className='bg-[#18181B0A] rounded-full px-[10px] inline-flex items-center gap-2 mb-4'>
-              <span className='w-[14px] h-[14px] rounded-full bg-red-500 block' />
-              <p className='text-secondary-foreground text-sm font-semibold'>
-                Project Failed
-              </p>
-            </div>
-
-            <div className='flex items-center gap-2 mb-3'>
-              <div className='w-8 h-8 rounded-full bg-gray-200' />
-              <div className='flex flex-col gap-1'>
-                <p className='text-sm font-semibold'>
-                  Project Name
-                </p>
-              </div>
-            </div>
-
-            <div className='flex items-center justify-between px-2 py-1.5 bg-[#18181B0A] rounded-lg mb-2'>
-              <p className='text-sm font-medium text-neutral-400'>PAYED</p>
-              <div className='flex items-center gap-1'>
-                <p className='text-sm text-muted-foreground font-bold'>2000</p>
-                <UsdtIcon width={16} height={16} />
-                <p className='text-sm text-muted-foreground font-medium'>USDT</p>
-              </div>
-            </div>
-
-            <div className='flex items-center justify-between px-2 py-2.5 bg-[#18181B0A] rounded-lg mb-4'>
-              <p className='text-sm font-bold text-foreground'>RECLAIM</p>
-              <div className='flex items-center gap-1'>
-                <p className='text-xl text-primary font-bold'>1000</p>
-                <UsdtIcon width={16} height={16} />
-                <p className='text-sm text-muted-foreground font-medium'>USDT</p>
-              </div>
-            </div>
-
-            <div className='flex items-center justify-end'>
-
-              <Button size='sm' className='px-6'>Reclaim now</Button>
-            </div>
-          </div>
         </div>
       </div>
-      {/* <Pagination totalCount={programData?.programs?.count ?? 0} pageSize={programPageSize} /> */}
+      <Pagination totalCount={investmentData?.investments?.count ?? 0} pageSize={programPageSize} />
     </div>
   );
 }
